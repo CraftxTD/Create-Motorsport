808dc75ee089fffd64458c5d1c022e6630e04bac
CraftxTD <craftxtd@gmail.com>
2026-07-29 18:00:20 -0400

    v0.13: started downflapblock

--- src/main/java/com/createmotorsport/block/DownFlapBlock.java
+++ src/main/java/com/createmotorsport/block/DownFlapBlock.java
@@ -1,22 +1,42 @@
 package com.createmotorsport.block;
 
+import com.createmotorsport.CreateMotorsport;
 import com.createmotorsport.block.entity.DownFlapBlockEntity;
 import com.mojang.serialization.MapCodec;
+import com.simibubi.create.foundation.placement.PoleHelper;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Predicate;
+
+import static net.minecraft.world.level.block.state.properties.BlockStateProperties.AXIS;
+import static net.minecraft.world.level.block.state.properties.BlockStateProperties.HORIZONTAL_FACING;
 
 public class DownFlapBlock extends HorizontalDirectionalBlock implements EntityBlock, IWrenchable {
    public static final MapCodec<DownFlapBlock> CODEC = simpleCodec(DownFlapBlock::new);
+   public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
    private static final Component CONTAINER_TITLE = Component.translatable("container.createmotorsport.down_flap");
 
     public DownFlapBlock(Properties properties) {
         super(properties);
+        registerDefaultState(defaultBlockState().setValue(POWERED, false));
     }
 
     @Override
@@ -28,5 +48,54 @@ public class DownFlapBlock extends HorizontalDirectionalBlock implements EntityB
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new DownFlapBlockEntity(pos, state);
+    }
+
+    @Override
+    public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos,
+                                boolean isMoving) {
+        if (level.isClientSide) {
+            return;
+        }
+
+
+        if (!level.getBlockTicks()
+                .willTickThisTick(pos, this))
+            level.scheduleTick(pos, this, 1);
+    }
+
+
+    @Override
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource r) {
+
+    }
+
+    @Override
+    public int getDirectSignal(BlockState blockState, BlockGetter blockAccess, BlockPos pos, Direction side) {
+        if (side != blockState.getValue(HORIZONTAL_FACING).getClockWise() || side != blockstate.getValue(HORIZONTAL_FACING).getCounterClockWise()) {
+            return getSignal(blockState, blockAccess, pos, side);
+        }
+    }
+
+    @Override
+    public boolean canConnectRedstone(BlockState state, BlockGetter world, BlockPos pos, Direction side) {
+        return side != null;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(HORIZONTAL_FACING, context.getHorizontalDirection());
+    }
+
+    @MethodsReturnNonnullByDefault
+    private static class PlacementHelper extends PoleHelper<Direction.Axis> {
+        private PlacementHelper() {
+            super(state -> state.getBlock() instanceof DownFlapBlock, state -> state.getValue(AXIS), AXIS);
+        }
+
+        @Override
+        public Predicate<ItemStack> getItemPredicate() {
+            return stack -> stack.is(CreateMotorsport.DOWN_FLAP_ITEM);
+        }
     }
 }
